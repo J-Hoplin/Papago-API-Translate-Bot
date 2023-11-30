c5d49500b793ea11ac40bab2125996f5f6e33903
J-hoplin1 <jhoplin7259@gmail.com>
2023-12-01 01:32:34 +0900

    Fix: Add ephemeral as default

--- src/commands/papago-translate.ts
+++ src/commands/papago-translate.ts
@@ -1,102 +1,126 @@
-import { CommandInteraction, PermissionFlagsBits, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
+import {
+  CommandInteraction,
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+} from "discord.js";
 import { DiscordCustomClient } from "../classes";
 import { Command, SlashCommandChoice, PapagoLanguageSet } from "../types";
 import { papagoRequest } from "../app/";
-import { translateResultFailEmbed, translateResultSuccessEmbed } from "../embeds";
+import {
+  translateResultFailEmbed,
+  translateResultSuccessEmbed,
+} from "../embeds";
 import logger from "../log/index";
 import { mapper } from "./papago-language-mapper";
 
 const supportedLanguages: PapagoLanguageSet[] = [
-    ...Object.keys(mapper).map((x: string): PapagoLanguageSet => {
-        return {
-            key: x,
-            name: mapper[x]
-        }
-    })
-]
-
+  ...Object.keys(mapper).map((x: string): PapagoLanguageSet => {
+    return {
+      key: x,
+      name: mapper[x],
+    };
+  }),
+];
 
 export const papagoTranslate: Command = {
-    data: new SlashCommandBuilder()
-        .setName('translate')
-        .setDescription('Translate your sentence using papago!')
-        .addStringOption((option): SlashCommandStringOption => {
-            return option.setName('from')
-                .setDescription("Translate from")
-                .setRequired(true)
-                .addChoices(...supportedLanguages.reduce((acc: SlashCommandChoice[], cur: PapagoLanguageSet, idx, src) => {
-                    acc.push({
-                        name: cur.name,
-                        value: cur.key
-                    })
-                    return acc
-                }, [])
-                )
-        })
-        .addStringOption((option): SlashCommandStringOption => {
-            return option.setName('to')
-                .setDescription('Translate to')
-                .setRequired(true)
-                .addChoices(...supportedLanguages.reduce((acc: SlashCommandChoice[], cur, idx, src) => {
-                    acc.push({
-                        name: cur.name,
-                        value: cur.key
-                    })
-                    return acc
-                }, [])
-                )
-        })
-        .addStringOption((option): SlashCommandStringOption => {
-            return option.setName('content')
-                .setDescription('Content you want to translate')
-                .setRequired(true)
-        })
-        .addStringOption((option): SlashCommandStringOption => {
-            return option
-                .setName("private")
-                .setDescription("Yes if you want to make question and response as public. Default is No")
-                .setRequired(false)
-                .addChoices(
-                    { name: 'Yes', value: 'Yes' },
-                    { name: 'No', value: 'No' }
-                )
-        })
-    ,
-    run: async (client: DiscordCustomClient, interaction: CommandInteraction) => {
-        const requestUserName = interaction.user.username
-        const from = interaction.options.get('from')?.value;
-        const to = interaction.options.get('to')?.value;
+  data: new SlashCommandBuilder()
+    .setName("translate")
+    .setDescription("Translate your sentence using papago!")
+    .addStringOption((option): SlashCommandStringOption => {
+      return option
+        .setName("from")
+        .setDescription("Translate from")
+        .setRequired(true)
+        .addChoices(
+          ...supportedLanguages.reduce(
+            (acc: SlashCommandChoice[], cur: PapagoLanguageSet, idx, src) => {
+              acc.push({
+                name: cur.name,
+                value: cur.key,
+              });
+              return acc;
+            },
+            []
+          )
+        );
+    })
+    .addStringOption((option): SlashCommandStringOption => {
+      return option
+        .setName("to")
+        .setDescription("Translate to")
+        .setRequired(true)
+        .addChoices(
+          ...supportedLanguages.reduce(
+            (acc: SlashCommandChoice[], cur, idx, src) => {
+              acc.push({
+                name: cur.name,
+                value: cur.key,
+              });
+              return acc;
+            },
+            []
+          )
+        );
+    })
+    .addStringOption((option): SlashCommandStringOption => {
+      return option
+        .setName("content")
+        .setDescription("Content you want to translate")
+        .setRequired(true);
+    })
+    .addStringOption((option): SlashCommandStringOption => {
+      return option
+        .setName("private")
+        .setDescription(
+          "Yes if you want to make question and response as public. Default is No"
+        )
+        .setRequired(false)
+        .addChoices({ name: "Yes", value: "Yes" }, { name: "No", value: "No" });
+    }),
+  run: async (client: DiscordCustomClient, interaction: CommandInteraction) => {
+    const requestUserName = interaction.user.username;
+    const from = interaction.options.get("from")?.value;
+    const to = interaction.options.get("to")?.value;
 
-        const fromName = mapper[from as string];
-        const toName = mapper[to as string];
-        const content = interaction.options.get('content')!.value as string;
-        const ephemeral = interaction.options.get('private')?.value === 'Yes'
+    const fromName = mapper[from as string];
+    const toName = mapper[to as string];
+    const content = interaction.options.get("content")!.value as string;
+    const ephemeral =
+      interaction.options.get("private")?.value === undefined ? true : false;
 
-        logger.info(`Translation Request(${requestUserName}) - ${from} → ${to} - Content : ${content}`)
-        const result = await papagoRequest(from as string, to as string, content)
-        try {
-            if (result.type === 'success') {
-                await interaction.reply({
-                    embeds: [
-                        await translateResultSuccessEmbed(fromName as string, toName as string, content, result.result),
-                    ],
-                    ephemeral
-                })
-            } else {
-                await interaction.editReply({
-                    embeds: [
-                        await translateResultFailEmbed(result.errormessage)
-                    ],
-                })
-            }
-        } catch (err) {
-            logger.error("Fail to embed - Too many contents")
-            await interaction.reply({
-                embeds: [
-                    await translateResultFailEmbed("😓 Content should be less than 1024 characters!")
-                ],
-                ephemeral
-            })
-        }
+    logger.info(
+      `Translation Request(${requestUserName}) - ${from} → ${to} - Content : ${content}`
+    );
+    const result = await papagoRequest(from as string, to as string, content);
+    try {
+      if (result.type === "success") {
+        await interaction.reply({
+          embeds: [
+            await translateResultSuccessEmbed(
+              fromName as string,
+              toName as string,
+              content,
+              result.result
+            ),
+          ],
+          ephemeral,
+        });
+      } else {
+        await interaction.editReply({
+          embeds: [await translateResultFailEmbed(result.errormessage)],
+        });
+      }
+    } catch (err) {
+      logger.error("Fail to embed - Too many contents");
+      await interaction.reply({
+        embeds: [
+          await translateResultFailEmbed(
+            "😓 Content should be less than 1024 characters!"
+          ),
+        ],
+        ephemeral,
+      });
     }
-}
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,57 @@
-import path from 'path'
-import { config } from 'dotenv'
+import { config } from "dotenv";
+import path from "path";
 config({
-    path: path.join(__dirname, "../.env")
-})
+  path: path.join(__dirname, "../.env"),
+});
 
 /**
  * Import Here
- * 
+ *
  */
-import * as Discord from './classes'
-import { Events, GatewayIntentBits, Collection, CommandInteraction, Interaction, ActivityType } from 'discord.js'
-import { ready, interactionCreate } from "./events"
-import logger from './log'
-import { papagoTranslate, pingcommand } from './commands'
 
-const client: Discord.DiscordCustomClient = new Discord.DiscordCustomClient({
+import { Events, GatewayIntentBits } from "discord.js";
+import * as Discord from "./classes";
+import { papagoTranslate, pingcommand } from "./commands";
+import { interactionCreate, ready } from "./events";
+import logger from "./log";
+
+const client: Discord.DiscordCustomClient = new Discord.DiscordCustomClient(
+  {
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessages
-    ]
-}, {
-    commands: [
-        papagoTranslate,
-        pingcommand
-    ]
-});
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMessages,
+    ],
+  },
+  {
+    commands: [papagoTranslate, pingcommand],
+  }
+);
 
-client.once(Events.ClientReady, info => {
-    logger.info(`I'm ready! Logged in as ${info.user.tag}`)
-})
+client.once(Events.ClientReady, (info) => {
+  logger.info(`I'm ready! Logged in as ${info.user.tag}`);
+});
 
 /**
  * Event Emitter
  */
 client.on(Events.ClientReady, () => {
-    ready(client)
-    client.user?.setStatus('online')
-    client.user?.setPresence({
-        status: 'online',
-        activities: [{
-            name: 'Discord Bot Boiler Plate!',
-        }]
-    })
-})
+  ready(client);
+  client.user?.setStatus("online");
+  client.user?.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: "Use /translate to translate!",
+      },
+    ],
+  });
+});
 
 // Slash Command is interaction. Add interaction event listener
 client.on(Events.InteractionCreate, (interaction) => {
-    interactionCreate(client, interaction)
-})
+  interactionCreate(client, interaction);
+});
 
 client.login(process.env.DISCORD_API);
